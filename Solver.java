0c4409672a1bc7f830e9b9f7cc796d18404f1f6c
Christian Trelenberg <christian.trelenberg@gmail.com>
2021-08-21 15:08:49 -0700

    Added row, col, box verifiers

--- Solver.java
+++ Solver.java
@@ -1,9 +1,12 @@
+import java.util.*;
+
 public class Solver {
 
-    private int[][] board = new int[3][3];
+    private int[][] board = new int[9][9];
+    private int[][] clue = new int[9][9];
 
-    public Solver() {
-        
+    public Solver(int[][] clue) {
+        this.clue = clue;
     }
 
     public void printBoard() {
@@ -18,8 +21,99 @@ public class Solver {
     public void fillBoard() {
         for (int i = 0; i < this.board.length; i++) {
             for (int j = 0; j < this.board[i].length; j++) {
-                    this.board[i][j] = 0;
+                    this.board[i][j] = this.clue[i][j];
             }
         }
     }
+
+    public boolean checkPuzzle() {
+        for (int i = 0; i < this.board.length; i++) {
+            if (!checkRow(i)) {
+                return false;
+            }
+        }
+        for (int i = 0; i < this.board[0].length; i++) {
+            if (!checkCol(i)) {
+                return false;
+            }
+        }
+        for (int i = 0; i < this.board.length; i++) {
+            for (int j = 0; j < this.board[i].length; j++) {
+                if (!checkSquare(i, j)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean checkRow(int row) {
+        HashMap<Integer, Integer> hmap = new HashMap<Integer, Integer>();
+        
+        for (int i = 0; i < this.board[row].length; i++) {
+            if (this.board[row][i] == 0) {
+                continue;
+            }
+
+            Integer c = hmap.get(this.board[row][i]);
+
+            if (c == null) {
+                hmap.put(this.board[row][i], 1);
+            }
+            else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean checkCol(int col) {
+        HashMap<Integer, Integer> hmap = new HashMap<Integer, Integer>();
+        
+        for (int i = 0; i < this.board.length; i++) {
+            if (this.board[i][col] == 0) {
+                continue;
+            }
+
+            Integer c = hmap.get(this.board[i][col]);
+
+            if (c == null) {
+                hmap.put(this.board[i][col], 1);
+            }
+            else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean checkSquare(int row, int col) {
+        HashMap<Integer, Integer> hmap = new HashMap<Integer, Integer>();
+
+        int squareRow = row / 3 * 3;
+        int squareCol = col / 3 * 3;
+
+        for (int i = squareRow; i < squareRow+3; i++) {
+            for (int j = squareCol; j < squareCol+3; j++) {
+                if (this.board[i][j] == 0) {
+                    continue;
+                }
+    
+                Integer c = hmap.get(this.board[i][j]);
+    
+                if (c == null) {
+                    hmap.put(this.board[i][j], 1);
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+
+
+        return true;
+    }
+
 }
